e30061e23502359765871156ed800a8db713ba10
gitdela <scriptingdela@gmail.com>
2023-05-16 12:03:59 +0000

    Add Header, Content & Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
+// import * as React from 'react';
 import './App.css';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
