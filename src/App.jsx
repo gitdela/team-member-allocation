a6a5541128057826d4adf364fac83456220768ea
gitdela <scriptingdela@gmail.com>
2023-05-17 16:11:00 +0000

    Add team members grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import * as React from 'react';
 import './App.css';
-import Content from './Content';
+import Employees from './Employees';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </div>
   );
